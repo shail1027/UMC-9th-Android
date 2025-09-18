13c4d04b39907c895058c0e188d95e6e0537a8cd
Yebin Lee <139226103+shail1027@users.noreply.github.com>
2025-09-18 23:46:11 +0900

    design : layout

--- Week01/Mission01/app/src/main/java/com/example/mission01/MainActivity.kt
+++ Week01/Mission01/app/src/main/java/com/example/mission01/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.mission01
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textViews: List<TextView>
+
+    private fun setClick(img: ImageView, text: TextView, message: String) {
+        img.setOnClickListener {
+            textViews.forEach { it.setTextColor(Color.BLACK) }
+            text.setTextColor(Color.parseColor("red"))
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val happyText = findViewById<TextView>(R.id.tvHappy)
+        val excitedText = findViewById<TextView>(R.id.tvExcited)
+        val normalText = findViewById<TextView>(R.id.tvNormal)
+        val anxiousText = findViewById<TextView>(R.id.tvAnxious)
+        val angryText = findViewById<TextView>(R.id.tvAngry)
+
+        textViews = listOf(happyText, excitedText, normalText, anxiousText, angryText)
+
+        setClick(findViewById(R.id.imgHappy), happyText, "행복한 하루")
+        setClick(findViewById(R.id.imgExcited), excitedText, "들뜬 하루")
+        setClick(findViewById(R.id.imgNormal), normalText, "평범한 하루")
+        setClick(findViewById(R.id.imgAnxious), anxiousText, "불안한 하루")
+        setClick(findViewById(R.id.imgAngry), angryText, "화나는 하루")
     }
 }
